144722e210d7075d1a82ed17bedc5cadda8f9b24
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-02-25 16:16:59 +0300

    add new file 24.js

---
+++ learn-javascript/24.js
@@ -0,0 +1,40 @@
+// Встроенный метод Math.random() возвращает случайное число от 0 (включительно) до 1 (но не включая 1)
+
+// Напишите функцию random(min, max), которая генерирует случайное число с плавающей точкой от min до max (но не включая max).
+
+// Пример работы функции:
+
+// alert( random(1, 5) ); // 1.2345623452
+// alert( random(1, 5) ); // 3.7894332423
+// alert( random(1, 5) ); // 4.3435234525
+
+function random(min, max) {
+    return min + Math.random() * (max - min);
+  }
+  
+  alert( random(1, 5) );
+  alert( random(1, 5) );
+  alert( random(1, 5) );
+
+// https://learn.javascript.ru/task/random-min-max
+
+
+// Напишите функцию randomInteger(min, max), которая генерирует случайное целое (integer) число от min до max (включительно).
+
+// Любое число из интервала min..max должно появляться с одинаковой вероятностью.
+
+// Пример работы функции:
+
+// alert( randomInteger(1, 5) ); // 1
+// alert( randomInteger(1, 5) ); // 3
+// alert( randomInteger(1, 5) ); // 5
+
+function randomInteger(min, max) {
+    // случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+  
+  alert( randomInteger(1, 3) );
+
+// https://learn.javascript.ru/task/random-int-min-max
