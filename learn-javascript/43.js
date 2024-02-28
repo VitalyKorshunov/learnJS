eb46a09dff18ff970fc06b378145d2620880b4ee
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-02-28 20:20:54 +0300

    add new file 43.js

---
+++ learn-javascript/43.js
@@ -0,0 +1,31 @@
+/*
+Оставить уникальные элементы массива
+
+Пусть arr – массив строк.
+
+Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+Например:
+
+function unique(arr) {
+  /- ваш код -/
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert( unique(strings) ); / кришна, харе, :-O
+*/
+
+
+function unique(arr) {
+    let result = [];
+
+    for(let str of arr) {
+        if (!result.includes(str)) {
+            result.push(str)
+        }
+    }
+    return result;
+}
