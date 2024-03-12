1c31a44b914071e31a92e8528e12ff87f8d30054
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-13 01:22:16 +0300

    add new solution 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -368,3 +368,23 @@ Example(Input --> Output):
 function reverseWords(str) {
   return str.split(' ').reverse().join(' ');
 }
+
+
+
+// 14. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/55edaba99da3a9c84000003b/train/javascript
+
+Find numbers which are divisible by given number
+
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6] 
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((elem) => elem % divisor == 0);
+}
