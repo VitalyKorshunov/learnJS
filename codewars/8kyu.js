f81407cca474461efe10b47f77329e4d0ab36786
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-16 23:28:49 +0300

    add new solution 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -388,3 +388,29 @@ Example(Input1, Input2 --> Output)
 function divisibleBy(numbers, divisor) {
   return numbers.filter((elem) => elem % divisor == 0);
 }
+
+
+
+// 15. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/563b74ddd19a3ad462000054/train/javascript
+
+Stringy Strings
+
+write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+the string should start with a 1.
+a string with size 6 should return :'101010'.
+with size 4 should return : '1010'.
+with size 12 should return : '101010101010'.
+The size will always be positive and will only use whole numbers.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function stringy(size) {
+  let str = '';
+  for (let i = 1; i <= size; i++) {
+    str += i % 2;
+  }
+  return str
+}
