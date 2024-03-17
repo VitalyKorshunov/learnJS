5eaeb24a5ded97526963883744b9b1f42bd4ed4c
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-17 23:35:16 +0300

    add new solution 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -636,3 +636,54 @@ function minimumSteps(numbers, value) {
 		if (sum >= value) return i;
 	}
 }
+
+
+
+// 14. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a4138acf28b82aa43000117/train/javascript
+
+Maximum Product
+
+Task
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+Input >> Output Examples
+adjacentElementsProduct([1, 2, 3]); ==> return 6
+Explanation:
+The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+Explanation:
+Max product obtained from multiplying 5 * 10  =  50 .
+
+adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])  ==>  return -14
+Explanation:
+The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent numbers in the array.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function adjacentElementsProduct(array) {
+	return Math.max(...array.map((n, i, arr) => i > 0 ? n * arr[i - 1] : n * arr[i + 1]))
+}
+
+// Best
+
+function adjacentElementsProduct(numbers) {
+	let currentMaxProduct = numbers[0] * numbers[1];
+
+	for (let i = 1; i < numbers.length - 1; i++) {
+		const productOfNextAdjacents = numbers[i] * numbers[i + 1];
+
+		if (productOfNextAdjacents > currentMaxProduct) {
+			currentMaxProduct = productOfNextAdjacents;
+		}
+	}
+
+	return currentMaxProduct;
+}
