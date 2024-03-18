1abceb8bda4e34fcb124aae089a6fc2c03a489fc
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-18 17:30:23 +0300

    add new solution 6kyu

--- codewars/6kyu.js
+++ codewars/6kyu.js
@@ -18,3 +18,61 @@ Don't forget the space after the closing parentheses!
 function createPhoneNumber(numbers) {
     return '(' + numbers.slice(0, 3).join('') + ')' + ' ' + numbers.slice(3, 6).join('') + '-' + numbers.slice(6, 10).join('');
 }
+
+
+
+// 2. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a946d9fba1bb5135100007c/train/javascript
+
+Transform To Prime
+
+Task :
+Given a List [] of n integers , find minimum number to be inserted in a list, 
+so that sum of all elements of list should equal the closest prime number .
+
+Notes
+List size is at least 2 .
+List's numbers will only positives (n > 0) .
+Repetition of numbers in the list could occur .
+The newer list's sum should equal the closest prime number .
+
+Input >> Output Examples
+1- minimumNumber ({3,1,2}) ==> return (1)
+Explanation:
+Since , the sum of the list's elements equal to (6) , the minimum number to be 
+inserted to transform the sum to prime number is (1) , which will make the sum 
+of the List equal the closest prime number (7) .
+2-  minimumNumber ({2,12,8,4,6}) ==> return (5)
+Explanation:
+Since , the sum of the list's elements equal to (32) , the minimum number to be 
+inserted to transform the sum to prime number is (5) , which will make the sum 
+of the List equal the closest prime number (37) .
+3- minimumNumber ({50,39,49,6,17,28}) ==> return (2)
+Explanation:
+Since , the sum of the list's elements equal to (189) , the minimum number to be 
+inserted to transform the sum to prime number is (2) , which will make the sum of the List equal the closest prime number (191) .
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function minimumNumber(numbers) {
+    let sumNumbers = numbers.reduce((sum, n) => sum + n);
+    let i = 0;
+
+    while (true) {
+        if (isEasyNum(sumNumbers)) return i;
+        i++;
+        sumNumbers++;
+    }
+}
+
+function isEasyNum(n) {
+    nSqrt = Math.trunc(Math.sqrt(n))
+
+    for (let i = 2; i <= nSqrt; i++) {
+        if (n % i == 0) return false
+    }
+
+    return true
+}
