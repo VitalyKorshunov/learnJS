004343b04b7a2de5556a5be21d76053d77e0a054
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-19 20:24:51 +0300

    add new solution 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -808,3 +808,53 @@ function balancedNum(number) {
 		: 'Not Balanced';
 
 }
+
+// 17. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a4d303f880385399b000001/train/javascript
+
+Strong Number (Special Numbers Series #2)
+
+Definition
+Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+
+For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Task
+Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+
+Notes
+Number passed is always Positive.
+Return the result as String
+Input >> Output Examples
+strong_num(1) ==> return "STRONG!!!!"
+Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+
+strong_num(123) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
+
+strong_num(2)  ==>  return "STRONG!!!!"
+Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
+
+strong_num(150) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong .
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function strong(n) {
+	let sumFactorials = [...n.toString()].reduce((sum, n) => sum + factorial(n), 0)
+
+	return (sumFactorials == n) ? "STRONG!!!!" : "Not Strong !!";
+}
+
+function factorial(n) {
+	let sum = 1;
+
+	for (let i = 2; i <= n; i++) {
+		sum *= i;
+	}
+
+	return sum;
+}
+
