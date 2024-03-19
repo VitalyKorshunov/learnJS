2b417b3d45c486938bc2687d9b7539449bfbd263
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-19 21:50:16 +0300

    add new solution 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -858,3 +858,38 @@ function factorial(n) {
 	return sum;
 }
 
+
+
+// 18. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a53a17bfd56cb9c14000003/train/javascript
+
+Disarium Number (Special Numbers Series #3)
+
+Definition
+Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+Task
+Given a number, Find if it is Disarium or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+Return the result as String
+Input >> Output Examples
+disariumNumber(89) ==> return "Disarium !!"
+Explanation:
+Since , 81 + 92 = 89 , thus output is "Disarium !!"
+disariumNumber(564) ==> return "Not !!"
+Explanation:
+Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function disariumNumber(n) {
+	return ([...`${n}`].reduce((sum, n, i) => sum + n ** [i + 1], 0) == n) ?
+		'Disarium !!' :
+		'Not !!';
+}
