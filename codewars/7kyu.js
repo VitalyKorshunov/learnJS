cb99e30d7ab2e3245a6b8d4f91efd35b603c6662
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-19 23:53:11 +0300

    add new solution 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -893,3 +893,64 @@ function disariumNumber(n) {
 		'Disarium !!' :
 		'Not !!';
 }
+
+// 19. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a54e796b3bfa8932c0000ed/train/javascript
+
+Jumping Number (Special Numbers Series #4)
+
+Definition
+Jumping number is the number that All adjacent digits in it differ by 1.
+
+Task
+Given a number, Find if it is Jumping or not .
+
+Notes
+Number passed is always Positive .
+
+Return the result as String .
+
+The difference between ‘9’ and ‘0’ is not considered as 1 .
+
+All single digit numbers are considered as Jumping numbers.
+
+Input >> Output Examples
+jumpingNumber(9) ==> return "Jumping!!"
+Explanation:
+It's single-digit number
+jumpingNumber(79) ==> return "Not!!"
+Explanation:
+Adjacent digits don't differ by 1
+jumpingNumber(23) ==> return "Jumping!!"
+Explanation:
+Adjacent digits differ by 1
+jumpingNumber(556847) ==> return "Not!!"
+Explanation:
+Adjacent digits don't differ by 1
+jumpingNumber(4343456) ==> return "Jumping!!"
+Explanation:
+Adjacent digits differ by 1
+jumpingNumber(89098) ==> return "Not!!"
+Explanation:
+Adjacent digits don't differ by 1
+jumpingNumber(32) ==> return "Jumping!!"
+Explanation:
+Adjacent digits differ by 1
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function jumpingNumber(n) {
+    n = n.toString()
+    if (n.length == 1) return 'Jumping!!';
+
+    for (let i = 0; i < n.length - 1; i++) {
+        let diff = n[i + 1] - n[i]
+
+        if (diff > 1 || diff < -1) {
+            return 'Not!!';
+        }
+    }
+    return 'Jumping!!';
+}
