625bb116f9380d6a489ee57b104d24ea4d8b9953
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-19 18:56:03 +0300

    add new solution 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -727,3 +727,84 @@ function minValue(values) {
 function minValue(values) {
 	return parseInt([...new Set(values)].sort((a, b) => a - b).join(''));
 }
+
+
+
+// 16. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a4e3782880385ba68000018/train/javascript
+
+Balanced Number (Special Numbers Series #1 )
+
+A balanced number is a number where the sum of digits to the left of the middle digit(s) and
+the sum of digits to the right of the middle digit(s) are equal.
+
+If the number has an odd number of digits, then there is only one middle digit. (For example,
+92645 has one middle digit, 6.) Otherwise, there are two middle digits. (For example, the
+middle digits of 1301 are 3 and 0.)
+
+The middle digit(s) should not be considered when determining whether a number is balanced or
+not, e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+
+The task
+Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced"
+accordingly. The passed number will always be positive.
+
+Examples
+7 ==> return "Balanced"
+Explanation:
+middle digit(s): 7
+sum of all digits to the left of the middle digit(s) -> 0
+sum of all digits to the right of the middle digit(s) -> 0
+0 and 0 are equal, so it's balanced.
+295591 ==> return "Not Balanced"
+Explanation:
+middle digit(s): 55
+sum of all digits to the left of the middle digit(s) -> 11
+sum of all digits to the right of the middle digit(s) -> 10
+11 and 10 are not equal, so it's not balanced.
+959 ==> return "Balanced"
+Explanation:
+middle digit(s): 5
+sum of all digits to the left of the middle digit(s) -> 9
+sum of all digits to the right of the middle digit(s) -> 9
+9 and 9 are equal, so it's balanced.
+27102983 ==> return "Not Balanced"
+Explanation:
+middle digit(s): 02
+sum of all digits to the left of the middle digit(s) -> 10
+sum of all digits to the right of the middle digit(s) -> 20
+10 and 20 are not equal, so it's not balanced.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function balancedNum(number) {
+	number = [...number.toString(10)];
+
+	if (number.length < 3) return 'Balanced';
+
+	if (number.length % 2) {
+		let leftNumsSum = number.slice(0, (number.length - 1) / 2).reduce((sum, n) => sum + +n, 0);
+		let rightNumsSum = number.slice((number.length + 1) / 2, number.length).reduce((sum, n) => sum + +n, 0);
+		return (leftNumsSum == rightNumsSum) ? 'Balanced' : 'Not Balanced';
+	} else {
+		let leftNumsSum = number.slice(0, (number.length) / 2 - 1).reduce((sum, n) => sum + +n, 0);
+		let rightNumsSum = number.slice((number.length) / 2 + 1, number.length).reduce((sum, n) => sum + +n, 0);
+		return (leftNumsSum == rightNumsSum) ? 'Balanced' : 'Not Balanced';
+	}
+}
+
+// Best
+
+function balancedNum(number) {
+
+	let str = `${number}`
+		, len = (str.length - (str.length % 2 ? -1 : -2)) / 2
+		, sum = digits => [...digits].reduce((a, b) => a + +b, 0);
+
+	return sum(str.slice(0, len)) === sum(str.slice(-len))
+		? 'Balanced'
+		: 'Not Balanced';
+
+}
