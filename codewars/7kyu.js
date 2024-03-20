9e38338114d7b0ebed04d08f5a2f2fb259e70f7d
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-20 18:31:26 +0300

    add solution #23 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1112,11 +1112,67 @@ First and last bits as set bits.
 // SOLUTION ✅✅✅✅✅✅
 
 function extraPerfect(n) {
-    let numbers = [];
+	let numbers = [];
 
-    for (let i = 1; i <= n; i += 2) {
-        numbers.push(i);
-    }
+	for (let i = 1; i <= n; i += 2) {
+		numbers.push(i);
+	}
 
-    return numbers;
+	return numbers;
+}
+
+
+
+// 23. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a87449ab1710171300000fd/train/javascript
+
+Tidy Number (Special Numbers Series #9)
+
+Definition
+A Tidy number is a number whose digits are in non-decreasing order.
+
+Task
+Given a number, Find if it is Tidy or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+
+Return the result as a Boolean
+
+Input >> Output Examples
+tidyNumber (12) ==> return (true)
+Explanation:
+The number's digits { 1 , 2 } are in non-Decreasing Order (i.e) 1 <= 2 .
+
+tidyNumber (32) ==> return (false)
+Explanation:
+The Number's Digits { 3, 2} are not in non-Decreasing Order (i.e) 3 > 2 .
+
+tidyNumber (1024) ==> return (false)
+Explanation:
+The Number's Digits {1 , 0, 2, 4} are not in non-Decreasing Order as 0 <= 1 .
+
+tidyNumber (13579) ==> return (true)
+Explanation:
+The number's digits {1 , 3, 5, 7, 9} are in non-Decreasing Order .
+
+tidyNumber (2335) ==> return (true)
+Explanation:
+The number's digits {2 , 3, 3, 5} are in non-Decreasing Order , Note 3 <= 3
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function tidyNumber(n) {
+	let newNum = n.toString(10).split('').sort((a, b) => a - b).join('');
+	return (newNum == n);
+}
+
+// Alternative
+
+function tidyNumber(n) {
+	return [...n += ""].sort().join('') == n
 }
