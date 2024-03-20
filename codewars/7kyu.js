a264cfa2ec422abb9363e44a7b11b799c41d8366
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-20 16:10:07 +0300

    add solution #20 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -942,15 +942,72 @@ Adjacent digits differ by 1
 // SOLUTION ✅✅✅✅✅✅
 
 function jumpingNumber(n) {
-    n = n.toString()
-    if (n.length == 1) return 'Jumping!!';
+	n = n.toString()
+	if (n.length == 1) return 'Jumping!!';
 
-    for (let i = 0; i < n.length - 1; i++) {
-        let diff = n[i + 1] - n[i]
+	for (let i = 0; i < n.length - 1; i++) {
+		let diff = n[i + 1] - n[i]
 
-        if (diff > 1 || diff < -1) {
-            return 'Not!!';
-        }
-    }
-    return 'Jumping!!';
+		if (diff > 1 || diff < -1) {
+			return 'Not!!';
+		}
+	}
+	return 'Jumping!!';
+}
+
+
+
+// 20. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a55f04be6be383a50000187/train/javascript
+
+Special Number (Special Numbers Series #5)
+
+Definition
+A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
+
+Given a number determine if it special number or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+The number passed will be positive (N > 0) .
+
+All single-digit numbers within the interval [1:5] are considered as special number.
+
+Input >> Output Examples
+specialNumber(2) ==> return "Special!!"
+Explanation:
+It's a single-digit number within the interval [1:5] .
+
+specialNumber(9) ==> return "NOT!!"
+Explanation:
+Although, it's a single-digit number but Outside the interval [1:5] .
+
+specialNumber(23) ==> return "Special!!"
+Explanation:
+All the number's digits formed from the interval [0:5] digits .
+
+specialNumber(39) ==> return "NOT!!"
+Explanation:
+Although, there is a digit (3) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+
+specialNumber(59) ==> return "NOT!!"
+Explanation:
+Although, there is a digit (5) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+
+specialNumber(513) ==> return "Special!!"
+specialNumber(709) ==> return "NOT!!"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function specialNumber(n) {
+	return [...n.toString(10)].every((num) => num <= 5) ? "Special!!" : "NOT!!";
+}
+
+// Alternative
+
+function specialNumber(n) {
+	return /[6-9]/.test(n) ? "NOT!!" : "Special!!"
 }
