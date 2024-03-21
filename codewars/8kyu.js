59479f0b7cfff2ba5ecfa3aae85af880a14530fa
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-21 23:36:43 +0300

    add solution #16 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -414,3 +414,30 @@ function stringy(size) {
   }
   return str
 }
+
+
+
+// 16. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
+
+Convert to Binary
+
+Task Overview
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+to_binary(1)  / should return 1 /
+to_binary(5)  / should return 101 /
+to_binary(11) / should return 1011 /
+Example:
+
+toBinary(1)  / should return 1 /
+toBinary(5)  / should return 101 /
+toBinary(11) / should return 1011 /
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function toBinary(n) {
+  return +n.toString(2);
+}
