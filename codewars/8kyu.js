2c38065ed8ef3a5a97d1a825d72d8f155f197db6
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-22 22:46:36 +0300

    add solution #18 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -470,3 +470,29 @@ function squareOrSquareRoot(array) {
     return sqrtN === Math.trunc(sqrtN) ? sqrtN : n ** 2;
   });
 }
+
+
+
+// 18. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/javascript
+
+Get Nth Even Number
+
+Return the Nth Even Number
+
+Example(Input --> Output)
+
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+The input will not be 0.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function nthEven(n) {
+  return n * 2 - 2;
+}
+
