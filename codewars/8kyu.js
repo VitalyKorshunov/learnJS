4b6041380e20659e6cd67dbe225e4a239edd69da
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-23 23:41:21 +0300

    add solution #21 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -531,3 +531,24 @@ Complete the square sum function so that it squares each number passed into it a
 function squareSum(numbers) {
   return numbers.reduce((sum, n) => sum += n ** 2, 0);
 }
+
+
+
+// 21. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+
+String repeat
+
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
