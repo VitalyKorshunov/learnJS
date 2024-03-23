f2667cf2b550415b842419bfcde7e3fbe11d7cc3
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-23 23:39:24 +0300

    add solution #20 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -496,6 +496,8 @@ function nthEven(n) {
   return n * 2 - 2;
 }
 
+
+
 // 19. ❓❓❓❓❓❓
 /*
 https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
@@ -513,3 +515,19 @@ function removeChar(str) {
   return str.slice(1, -1)
 };
 
+
+
+// 20. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+Square(n) Sum
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function squareSum(numbers) {
+  return numbers.reduce((sum, n) => sum += n ** 2, 0);
+}
