f4e52f536f1f58aa98fc083aecc0fbfbf4b8e306
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-24 16:09:45 +0300

    add solution #24 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -614,3 +614,35 @@ function converter(mpg) {
 
   return +(mpg * MILE_TO_KILOMETRES / IMPERIAL_GALLON_TO_LITRES).toFixed(2);
 }
+
+
+
+// 24. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/train/javascript
+
+The falling speed of petals
+
+When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. 
+The petals start to fall in late April.
+
+Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it 
+takes 80 seconds for the petal to reach the ground from a certain branch.
+
+Write a function that receives the speed (in cm/s) of a petal as input, and returns the 
+time it takes for that petal to reach the ground from the same branch.
+
+Notes:
+
+The movement of the petal is quite complicated, so in this case we can see the velocity 
+as a constant during its falling.
+Pay attention to the data types.
+If the initial velocity is non-positive, the return value should be 0
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function sakuraFall(v) {
+  const s = 400;
+  return (v > 0) ? s / v : 0;
+}
