315277028ebbdce34e244250e4bdec4af61645a7
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-24 15:57:46 +0300

    add solution #23 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -585,3 +585,32 @@ var summation = function (num) {
 function summation(num) {
   return num * (num + 1) / 2;
 }
+
+
+
+// 23. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/557b5e0bddf29d861400005d/train/javascript
+
+Miles per gallon to kilometers per liter
+
+Sometimes, I want to quickly be able to convert miles per imperial gallon (mpg) into kilometers per liter (kpl).
+
+Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+Make sure to round off the result to two decimal points.
+
+Some useful associations relevant to this kata:
+
+1 Imperial Gallon = 4.54609188 litres
+1 Mile = 1.609344 kilometres
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function converter(mpg) {
+  const IMPERIAL_GALLON_TO_LITRES = 4.54609188;
+  const MILE_TO_KILOMETRES = 1.609344;
+
+  return +(mpg * MILE_TO_KILOMETRES / IMPERIAL_GALLON_TO_LITRES).toFixed(2);
+}
