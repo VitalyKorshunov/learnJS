727d65f3417abf2ff5aa0aba278d96eaf7f31368
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-24 20:13:53 +0300

    add solution #3 6kyu

--- codewars/6kyu.js
+++ codewars/6kyu.js
@@ -76,3 +76,63 @@ function isEasyNum(n) {
 
     return true
 }
+
+
+
+// 3. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a99a03e4a6b34bb3c000124/train/javascript
+
+Definition (Primorial Of a Number)
+
+Is similar to factorial of a number, In primorial, not all the natural numbers get multiplied, only prime numbers 
+are multiplied to calculate the primorial of a number. It's denoted with P# and it is the product of the first n prime numbers.
+
+Task
+Given a number N , calculate its primorial.!alt!alt
+Notes
+Only positive numbers will be passed (N > 0) .
+Input >> Output Examples:
+1- numPrimorial (3) ==> return (30)
+Explanation:
+Since the passed number is (3) ,Then the primorial should obtained by multiplying 2 * 3 * 5 = 30 .
+
+Mathematically written as , P3# = 30 .
+2- numPrimorial (5) ==> return (2310)
+Explanation:
+Since the passed number is (5) ,Then the primorial should obtained by multiplying  2 * 3 * 5 * 7 * 11 = 2310 .
+
+Mathematically written as , P5# = 2310 .
+3- numPrimorial (6) ==> return (30030)
+Explanation:
+Since the passed number is (6) ,Then the primorial should obtained by multiplying  2 * 3 * 5 * 7 * 11 * 13 = 30030 .
+
+Mathematically written as , P6# = 30030 .
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function numPrimorial(num) {
+    let primes = getPrimes(num);
+    return primes.reduce((acc, n) => acc * n, 1);
+}
+
+function getPrimes(n) {
+    let result = [];
+    for (let i = 2; result.length < n; i++) {
+        if (isPrime(i)) result.push(i)
+    }
+    return result;
+}
+
+function isPrime(n) {
+    let sqrt = Math.trunc(Math.sqrt(n));
+
+    if (n < 2) return false;
+
+    for (let i = 2; i <= sqrt; i++) {
+        if (!(n % i)) return false;
+    }
+
+    return true;
+}
