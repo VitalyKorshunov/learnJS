76b03cc36eb3128fc8b316d9a215e725841b1741
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-24 21:05:26 +0300

    add solution #4 6kyu

--- codewars/6kyu.js
+++ codewars/6kyu.js
@@ -136,3 +136,42 @@ function isPrime(n) {
 
     return true;
 }
+
+
+
+// 4. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5848565e273af816fb000449/train/javascript
+
+Encrypt this!
+
+Acknowledgments:
+I thank yvonne-liu for the idea and for the example tests :)
+
+Description:
+Encrypt this!
+
+You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+Your message is a string containing space separated words.
+You need to encrypt each word in the message using the following rules:
+The first letter must be converted to its ASCII code.
+The second letter must be switched with the last letter
+Keepin' it simple: There are no special characters in the input.
+Examples:
+encryptThis("Hello") === "72olle"
+encryptThis("good") === "103doo"
+encryptThis("hello world") === "104olle 119drlo"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+var encryptThis = function (text) {
+    return text.split(' ').map((word) => word[0].charCodeAt(0) + (word.length > 1 ? word.slice(-1) : '') + word.slice(2, -1) + (word.length > 2 ? word.charAt(1) : '')).join(' ');
+}
+
+// Best
+
+const encryptThis = text =>
+    text.replace(/\b\w(\w?)(\w*?)(\w?)\b/g, (word, a, mid, b) =>
+        word.charCodeAt(0) + b + mid + a);
