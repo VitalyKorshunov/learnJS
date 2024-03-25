2a0b6354e6a2a7a6d4f22668591aafded7759cc1
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-25 20:06:57 +0300

    add solution #25 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1201,3 +1201,39 @@ Happy Coding!
 function squareDigits(num) {
 	return +[...(`${num}`)].reduce((str, n) => str + n ** 2, '');
 }
+
+
+
+// 25. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+
+Highest and Lowest
+
+In this little assignment you are given a string of space separated numbers, and have to return 
+the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function highAndLow(numbers) {
+	numbers = numbers.split(' ')
+	let min = +numbers[0];
+	let max = +numbers[0];
+
+	numbers.forEach((num) => {
+		min = (num < min) ? +num : min;
+		max = (num > max) ? +num : max;
+	});
+
+	return `${max} ${min}`;
+}
