e7ed754448124b5be58ed2794cf85bd0311373aa
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-26 23:14:47 +0300

    add solution #28 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1299,3 +1299,26 @@ function getMiddle(s) {
 		? s[s.length / 2 - 1] + s[s.length / 2]
 		: s[(s.length - 1) / 2];
 }
+
+
+
+// 28. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+
+Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function accum(s) {
+	return s.split('').map((letter, i) => letter.toUpperCase() + letter.repeat(i).toLowerCase()).join('-');
+}
