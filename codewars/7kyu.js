f33c7efff197e664c76fb0f78ddeb54273d261ca
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-03-28 13:22:27 +0300

    add solution #30 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1361,3 +1361,30 @@ Examples
 var isSquare = function (n) {
 	return Number.isInteger(Math.sqrt(n))
 }
+
+
+
+// 30. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript
+
+Isograms
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function isIsogram(str) {
+	str = str.toLowerCase();
+	let setStr = new Set(str);
+	return str.length === setStr.size;
+}
