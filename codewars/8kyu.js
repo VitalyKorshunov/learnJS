eebd01d1200d27eb5a12411c586597421c2e05c6
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-04 19:29:19 +0300

    add solution #27 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -723,3 +723,27 @@ elevator(0, 2, 1); // => "right"
 function elevator(left, right, call) {
   return (Math.abs(call - right) <= Math.abs(call - left)) ? 'right' : 'left';
 }
+
+
+
+// 27. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+Remove String Spaces
+
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples:
+
+Input -> Output
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function noSpace(x) {
+  return x.split(' ').join('');
+}
