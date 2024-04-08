c10057956d0d44fee12c74ea831ec5fb309e5b8f
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-08 19:29:34 +0300

    add solution #33 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1429,3 +1429,52 @@ function sumTwoSmallestNumbers(numbers) {
 	numbersCopy.sort((a, b) => a - b);
 	return numbersCopy[0] + numbersCopy[1];
 }
+
+
+
+// 33. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+
+Beginner Series #3 Sum of Numbers
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers 
+between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+Your function should only return a number, not the explanation about how you get that number.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function getSum(a, b) {
+	let sum = 0;
+
+	if (a === b) {
+		return a;
+	} else if (a > b) {
+		[a, b] = [b, a];
+	}
+
+	for (let i = a; i <= b; i++) {
+		sum += i;
+	}
+
+	return sum;
+}
+
+// Best
+
+const GetSum = (a, b) => {
+	let min = Math.min(a, b),
+		max = Math.max(a, b);
+	return (max - min + 1) * (min + max) / 2;
+}
