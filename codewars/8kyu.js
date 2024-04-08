8501ecb7b52f261bfb847a48fd496c1c1f7d0ea1
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-08 18:38:30 +0300

    add solution #28 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -747,3 +747,27 @@ Input -> Output
 function noSpace(x) {
   return x.split(' ').join('');
 }
+
+
+
+// 28. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/javascript
+
+Beginner Series #1 School Paperwork
+
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' 
+classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+Example:
+n= 5, m=5: 25
+n=-5, m=5:  0
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function paperwork(n, m) {
+  return (n < 0 || m < 0) ? 0 : n * m;
+}
