e27c83243aef57f004c7c7f7ff92125ee02b169a
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-11 23:59:04 +0300

    add solution #34 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1478,3 +1478,36 @@ const GetSum = (a, b) => {
 		max = Math.max(a, b);
 	return (max - min + 1) * (min + max) / 2;
 }
+
+
+
+// 34. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/57f609022f4d534f05000024/train/javascript
+
+Find the stray number
+
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function stray(numbers) {
+	const obj = {};
+
+	numbers.forEach((a) => {
+		return (obj[a] === undefined) ? obj[a] = 1 : obj[a]++;
+	});
+
+	for (let num in obj) {
+		if (obj[num] == 1) return +num;
+	}
+}
