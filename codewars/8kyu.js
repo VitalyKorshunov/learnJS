608c3bddf33ed6c3e38012c63b73fa1eb2f6334e
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-14 20:58:53 +0300

    add solution #32 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -876,3 +876,34 @@ function points(games) {
       (elem[0] < elem[2]) ? 0 : 1,
     0)
 }
+
+
+
+// 32. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+
+Basic Mathematical Operations
+
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function basicOp(operation, value1, value2) {
+  return (
+    (operation === '+') ? value1 + value2 :
+      (operation === '-') ? value1 - value2 :
+        (operation === '*') ? value1 * value2 :
+          (operation === '/') ? value1 / value2 : 'Operation error'
+  )
+}
