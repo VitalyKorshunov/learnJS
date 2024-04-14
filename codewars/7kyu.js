694f06fc543e1c34d2447df02cad729eee0df028
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-14 20:02:35 +0300

    add solution #35 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1511,3 +1511,60 @@ function stray(numbers) {
 		if (obj[num] == 1) return +num;
 	}
 }
+
+
+
+// 35. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/56f3a1e899b386da78000732/train/javascript
+
+Parts of a list
+
+Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+Examples of returns in different languages:
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], 
+["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+
+Note
+You can see other examples for each language in "Your test cases"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function partlist(arr) {
+	let newArr = [];
+	for (let i = 1; i < arr.length; i++) {
+		newArr.push([arr.slice(0, i).join(' '), arr.slice(i).join(' ')]);
+	}
+
+	return newArr;
+}
+
+
+
+// 35. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/56f3a1e899b386da78000732/train/javascript
+
+Parts of a list
+
+Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+Examples of returns in different languages:
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], 
+["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+
+Note
+You can see other examples for each language in "Your test cases"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
