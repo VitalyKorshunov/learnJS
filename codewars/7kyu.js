a67d7cb4074257467658db92882e3a7431d02697
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-18 13:00:42 +0300

    add solution #39 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1664,3 +1664,38 @@ function replicate(times, number) {
 		return [number].concat(replicate(times - 1, number));
 	}
 }
+
+
+
+// 39. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/569512b7707bc1b88200002f/train/javascript
+
+Recursion #2 - Fibonacci
+
+ Do you know recursion?
+This is a kata series that you can only solve using recursion.
+##2 - Fibonacci number
+
+In mathematical terms, the sequence f(n) of fibonacci numbers is defined by the recurrence relation
+
+f(n) = f(n-1) + f(n-2)
+
+with seed values
+
+f(1) = 1 and f(2) = 1
+
+#Your task
+
+You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+const fibonacci = n => {
+	if (n <= 2) {
+		return 1;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+};
