a9c2a3f734acf5abbcef7c88273791b1c8738f3b
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-19 13:25:26 +0300

    add solution #35 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -984,8 +984,32 @@ with only 1 element, return 0.
 
 function sumArray(array) {
   if (!(Array.isArray(array) && array.length > 1)) return 0
-  
+
   const arrayCopy = array.slice();
-  
+
   return arrayCopy.sort((a, b) => a - b).slice(1, -1).reduce((acc, num) => acc += num, 0);
+}
+
+
+
+// 35. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+Do I get a bonus?
+
+It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive 
+only his stated salary.
+
+Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), 
+"$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function bonusTime(salary, bonus) {
+  return "\u00A3" + ((bonus) ? salary * 10 : salary);
 }
