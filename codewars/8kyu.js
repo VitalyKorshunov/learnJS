d8378e88d067a78663adf5f29865b7194ceaf20b
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-19 12:59:45 +0300

    add solution #33 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -907,3 +907,54 @@ function basicOp(operation, value1, value2) {
           (operation === '/') ? value1 / value2 : 'Operation error'
   )
 }
+
+
+
+// 33. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
+
+Rock Paper Scissors!
+
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+const rps = (p1, p2) => {
+  if (p1 === p2) {
+    return 'Draw!';
+  } else if (
+    (p1 === 'scissors' && p2 === "paper") ||
+    (p1 === 'paper' && p2 === 'rock') ||
+    (p1 === 'rock' && p2 === 'scissors')) {
+    return 'Player 1 won!';
+  } else {
+    return 'Player 2 won!';
+  }
+};
+
+// Best
+
+const rps2 = (p1, p2) => {
+  const rules = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+  };
+
+  if (p1 === p2) {
+    return "Draw!";
+  } else if (rules[p1] === p2) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+};
