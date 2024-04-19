40d2c4c75f373ca2d1699d991810415185d7d751
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-19 13:22:54 +0300

    add solution #34 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -958,3 +958,34 @@ const rps2 = (p1, p2) => {
     return "Player 2 won!";
   }
 };
+
+
+
+// 34. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/javascript
+
+Sum without highest and lowest number
+
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list 
+with only 1 element, return 0.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function sumArray(array) {
+  if (!(Array.isArray(array) && array.length > 1)) return 0
+  
+  const arrayCopy = array.slice();
+  
+  return arrayCopy.sort((a, b) => a - b).slice(1, -1).reduce((acc, num) => acc += num, 0);
+}
