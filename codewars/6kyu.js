83dda3da1a115679fd79413bad8ee89bf9a55593
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-20 23:26:48 +0300

    add solution #7 6kyu

--- codewars/6kyu.js
+++ codewars/6kyu.js
@@ -297,3 +297,31 @@ function findOdd(A) {
         if (value % 2) return key;
     }
 }
+
+
+
+// 7. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
+
+Stop gninnipS My sdroW!
+
+Write a function that takes in a string of one or more words, and returns the same string, but with all 
+words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will 
+consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function spinWords(string) {
+    return (string.split(' ')
+        .map((word) => word.length >= 5 ? word.split('').reverse().join('') : word)
+        .join(' ')
+    );
+}
