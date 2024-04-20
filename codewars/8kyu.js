8d1ddc0879fdfbe65b8a288f38befb7125c4a48f
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-20 23:03:17 +0300

    add solution #36 8kyu

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -1013,3 +1013,34 @@ Return the total figure the individual will receive as a string prefixed with "�
 function bonusTime(salary, bonus) {
   return "\u00A3" + ((bonus) ? salary * 10 : salary);
 }
+
+
+
+// 36. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+Find Maximum and Minimum Values of a List
+
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+Notes
+You may consider that there will not be any empty arrays/vectors.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+var min = function (list) {
+
+  return list.slice().sort((a, b) => a - b)[0];
+}
+
+var max = function (list) {
+
+  return list.slice().sort((a, b) => b - a)[0];
+}
