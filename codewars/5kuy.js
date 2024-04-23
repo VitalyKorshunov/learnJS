ec19c8d90491b3896e99422a94872ffff5aa1ba7
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-23 12:32:13 +0300

    add solution #2 5kyu

--- codewars/5kuy.js
+++ codewars/5kuy.js
@@ -21,3 +21,39 @@ function pigIt(str) {
         .map((word) => word.slice(1) + word.slice(0, 1) + ((word === '?' || word === '!' || word === '.') ? '' : 'ay'))
         .join(' ');
 }
+
+
+
+// 2. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/52685f7382004e774f0001f7/train/javascript
+
+Human Readable Time
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a 
+human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function humanReadable(seconds) {
+    if (seconds > 359999) return '99:59:59';
+
+    const ss = formatTime(seconds % 60);
+    const mm = formatTime(Math.trunc(seconds / 60 % 60));
+    const hh = formatTime(Math.trunc(seconds / 3600 % 100));
+
+    return `${hh}:${mm}:${ss}`
+}
+
+function formatTime(time) {
+    time = time.toString();
+    return (time.length === 1) ? '0' + time : time;
+}
