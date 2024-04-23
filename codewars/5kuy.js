e3e73d309d09ac718e6cfea7c73734946ae38cd5
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-23 11:34:32 +0300

    add solution #1 5kyu

---
+++ codewars/5kuy.js
@@ -0,0 +1,23 @@
+//Задачи уровня 5kyu
+
+// 1. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+
+Simple Pig Latin
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function pigIt(str) {
+    return str.split(' ')
+        .map((word) => word.slice(1) + word.slice(0, 1) + ((word === '?' || word === '!' || word === '.') ? '' : 'ay'))
+        .join(' ');
+}
