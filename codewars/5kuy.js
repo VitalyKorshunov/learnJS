ed66a31a323dced237e3f0f2b7e1771a65a57d50
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-24 15:11:34 +0300

    add solution #3 5kyu

--- codewars/5kuy.js
+++ codewars/5kuy.js
@@ -57,3 +57,41 @@ function formatTime(time) {
     time = time.toString();
     return (time.length === 1) ? '0' + time : time;
 }
+
+
+
+// 3. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/513e08acc600c94f01000001/train/javascript
+
+
+RGB To Hex Conversion
+
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a 
+hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that 
+fall out of that range must be rounded to the closest valid value.
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+Examples (input --> output):
+255, 255, 255 --> "FFFFFF"
+255, 255, 300 --> "FFFFFF"
+0, 0, 0       --> "000000"
+148, 0, 211   --> "9400D3"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function rgb(r, g, b) {
+    return convertToHex(r) + convertToHex(g) + convertToHex(b);
+}
+
+function convertToHex(num) {
+    let hex = range(num);
+    hex = hex.toString(16);
+    return (hex.length === 1) ? '0' + hex.toUpperCase() : hex.toUpperCase();
+}
+
+function range(num) {
+    return ((num < 0) ? '00' :
+        (num > 255) ? 'FF' : num);
+}
