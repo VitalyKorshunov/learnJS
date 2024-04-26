81eae040165ab0e82d92295d2be3bdb3f93f9db7
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-26 15:45:05 +0300

    add solution #42 7kyu

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -1753,3 +1753,41 @@ function disemvowel(str) {
 			.filter((letter) => !(['a', 'o', 'e', 'i', 'u'].includes(letter.toLowerCase()))).join(''))
 		.join(' '));
 }
+
+
+
+// 42. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
+
+Credit Card Mask
+
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to
+your most secret question is still correct. However, since someone could look over your shoulder, you don't
+want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples (input --> output):
+"4556364607935616" --> "############5616"
+		 "64607935616" -->      "#######5616"
+							 "1" -->                "1"
+								"" -->                 ""
+
+/ "What was the name of your first pet?"
+"Skippy" --> "##ippy"
+"Nananananananananananananananana Batman!" --> "####################################man!"
+
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function maskify(cc) {
+	return (cc.length < 5) ? cc : '#'.repeat(cc.length - 4) + cc.slice(-4);
+}
+
+// BEST 
+
+function maskify(cc) {
+	return cc.slice(-4).padStart(cc.length, '#')
+}
