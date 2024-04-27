a96ee679f97df080fefaf6a5f51b724d6c4353bd
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-27 22:19:24 +0300

    add solution #37 - 8kyu

--- codewars/js/8kyu.js
+++ codewars/js/8kyu.js
@@ -1044,3 +1044,24 @@ var max = function (list) {
 
   return list.slice().sort((a, b) => b - a)[0];
 }
+
+
+
+// 37. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/59342039eb450e39970000a6/train/javascript
+
+Count Odd Numbers below n
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+Expect large Inputs!
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function oddCount(n) {
+  return (n % 2) ? (n - 1) / 2 : n / 2;
+}
