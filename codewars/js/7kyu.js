8beef47edada45a3049fc97ddf0975865517b3d2
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-29 23:46:38 +0300

    add solution #44 - 7kyu

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -1832,3 +1832,25 @@ function inverseSlice(items, a, b) {
 	itemsCopy.splice(a, b - a)
 	return itemsCopy;
 }
+
+
+
+// 44. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+
+String ends with?
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function solution(str, ending) {
+	return str.endsWith(ending)
+}
