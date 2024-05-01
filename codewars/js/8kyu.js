466bc33106e0ca229e538fd8fb8cb4b10243dc2a
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-02 00:00:44 +0300

    add solution #38 - 8kyu

--- codewars/js/8kyu.js
+++ codewars/js/8kyu.js
@@ -1065,3 +1065,30 @@ Expect large Inputs!
 function oddCount(n) {
   return (n % 2) ? (n - 1) / 2 : n / 2;
 }
+
+
+
+// 38. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+Century From Year
+
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+2742 --> 28
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
