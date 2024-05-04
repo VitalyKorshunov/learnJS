f15e6a07e71323bb3b37ee476d4e844c70007c67
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-04 20:08:26 +0300

    edit solution #18 - 6kyu

--- codewars/js/6kyu.js
+++ codewars/js/6kyu.js
@@ -669,3 +669,17 @@ var uniqueInOrder = function (iterable) {
 
 	return result;
 }
+
+// ver 2.
+
+var uniqueInOrder = function (iterable) {
+	const result = [];
+
+	for (let i = 0; i < iterable.length; i++) {
+		if (i === 0 || iterable[i] !== iterable[i - 1]) {
+			result.push(iterable[i])
+		}
+	}
+
+	return result;
+}
