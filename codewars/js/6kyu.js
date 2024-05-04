f0634450807b1d1860a01b2a0247616440b86aca
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-04 19:59:23 +0300

    add solution #18 - 6kyu

--- codewars/js/6kyu.js
+++ codewars/js/6kyu.js
@@ -631,3 +631,41 @@ function toCamelCase(str) {
 		.map((word, i) => (i === 0) ? word : word[0].toUpperCase() + word.slice(1))
 		.join('');
 }
+
+
+
+// 18. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
+
+Unique In Order
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
+without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+var uniqueInOrder = function (iterable) {
+	let result;
+
+	if (Array.isArray(iterable)) {
+		result = iterable
+			.reduce((acc, elem, i) => (elem === iterable[i + 1]) ? acc : acc += elem, '')
+			.split('')
+			.map((num) => (isNaN(num)) ? num : +num);
+	} else {
+		result = iterable
+			.split('')
+			.reduce((acc, elem, i) => (elem === iterable[i + 1]) ? acc : acc += elem, '')
+			.split('');
+	}
+
+	return result;
+}
