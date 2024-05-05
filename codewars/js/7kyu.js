6a3db20e9e7a1f01f21b5fc397aa59b885a8c9d0
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-05 16:01:43 +0300

    add solution #45 - 7kyu

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -1854,3 +1854,51 @@ solution('abc', 'd') // returns false
 function solution(str, ending) {
 	return str.endsWith(ending)
 }
+
+
+
+// 45. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/javascript
+
+Anagram Detection
+
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+let isAnagram = function (test, original) {
+	if (test.length !== original.length) return false;
+
+	test = test.toLowerCase().split('');
+	original = original.toLowerCase().split('');
+
+	return original
+		.every((letter) => {
+			if (test.includes(letter)) {
+				test[test.indexOf(letter)] = '';
+				return true;
+			} else {
+				return false;
+			}
+		});
+};
+
+// Best
+
+var isAnagram1 = function (test, original) {
+	test = test.toLowerCase().split('').sort().join('');
+	original = original.toLowerCase().split('').sort().join('');
+
+	return test === original;
+};
