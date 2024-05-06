f0fdf6e06ffca4e56a8b0b6573c6fff4e5743a7c
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-06 17:00:01 +0300

    add solution #9 - 8kyu sql

--- codewars/sql/8kuy.sql
+++ codewars/sql/8kuy.sql
@@ -180,3 +180,32 @@ WHERE
   age > 50
 ORDER BY
   age DESC;
+
+/* 9. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/5802e32dd8c944e562000020/train/sql
+ 
+ SQL Basics: Simple JOIN
+ 
+ For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
+ 
+ products table schema
+ id
+ name
+ isbn
+ company_id
+ price
+ companies table schema
+ id
+ name
+ You should return all product fields as well as the company name as "company_name".
+ 
+ NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+ */
+-- SOLUTION ✅✅✅✅✅✅
+SELECT
+  p.*,
+  c.name AS company_name
+FROM
+  products AS p
+  JOIN companies AS c ON p.company_id = c.id;
