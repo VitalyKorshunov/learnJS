a3cdef8e7eb9c53038a4630a4fa2bc069e408314
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-07 15:14:50 +0300

    add solution #12 - 8kyu sql

--- codewars/sql/8kuy.sql
+++ codewars/sql/8kuy.sql
@@ -234,7 +234,8 @@ WHERE
  
  Register for the Party (SQL for Beginners #3)
  
- You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+ You received an invitation to an amazing party. Now you need to write an insert statement to add yourself 
+ to the table of participants.
  
  participants table schema
  
@@ -250,9 +251,36 @@ WHERE
 INSERT INTO
   participants(name, age, attending)
 VALUES
-('Vitaly', 29, true);
+  ('Vitaly', 29, true);
 
 SELECT
   *
 FROM
   participants;
+
+/* 12. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/5910b0d378cc2ba91400000b/train/sql 
+ 
+ Collect Tuition (SQL for Beginners #4)
+ 
+ You are working for a local school, and you are responsible for collecting tuition from students. 
+ You have a list of all students, some of them have already paid tution and some haven't. Write a select 
+ statement to get a list of all students who haven't paid their tuition yet. The list should include all 
+ the data available about these students.
+ 
+ students table schema
+ 
+ name (string)
+ age (integer)
+ semester (integer)
+ mentor (string)
+ tuition_received (Boolean)
+ NOTE: Your solution should use pure SQL. Ruby is used within the test cases just to validate your answer.
+ */
+SELECT
+  *
+FROM
+  students
+WHERE
+  tuition_received = false;
