cd96f3f88a0a03835ddd99dfd5055c191b39c7fa
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-07 17:18:56 +0300

    edit solution #19 - 6kyu

--- codewars/js/6kyu.js
+++ codewars/js/6kyu.js
@@ -683,3 +683,30 @@ var uniqueInOrder = function (iterable) {
 
 	return result;
 }
+
+
+
+// 19. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript
+
+Find the unique number
+
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function findUniq(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] !== arr[i + 1]) {
+			return (arr[i + 1] === arr[i + 2]) ? arr[i] : arr[i + 1];
+		}
+	}
+}
