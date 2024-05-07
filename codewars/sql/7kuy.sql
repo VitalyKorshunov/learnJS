4f86be53486bc60979197daa5c25247df9fd48b3
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-07 13:44:38 +0300

    add solution #3 - 7kyu sql

--- codewars/sql/7kuy.sql
+++ codewars/sql/7kuy.sql
@@ -53,3 +53,31 @@ FROM
   people
 GROUP BY
   age;
+
+/* 3. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/580918e24a85b05ad000010c/train/sql
+ 
+ SQL Basics: Simple JOIN with COUNT
+ 
+ For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys
+ 
+ people table schema
+ id
+ name
+ toys table schema
+ id
+ name
+ people_id
+ You should return all people fields as well as the toy count as "toy_count".
+ 
+ NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+ */
+SELECT
+  people.*,
+  COUNT (*) AS toy_count
+FROM
+  people
+  JOIN toys ON people.id = toys.people_id
+GROUP BY
+  people.id;
