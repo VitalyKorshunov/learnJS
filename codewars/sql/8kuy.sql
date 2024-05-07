87409fcda5cf739d857c4de6b8fb10c0005ab78f
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-07 15:08:21 +0300

    add solution #11 - 8kyu sql

--- codewars/sql/8kuy.sql
+++ codewars/sql/8kuy.sql
@@ -227,3 +227,32 @@ FROM
   travelers
 WHERE
   country NOT IN ('Canada', 'Mexico', 'USA');
+
+/* 11. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/590cc86f7557c0494000007e/train/sql
+ 
+ Register for the Party (SQL for Beginners #3)
+ 
+ You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+ 
+ participants table schema
+ 
+ name (string)
+ age (integer)
+ attending (boolean)
+ NOTES:
+ 
+ Since alcohol will be served, you can only attend if you are 21 or older
+ You can't attend if the attending column returns anything but true
+ NOTE: Your solution should use pure SQL. Ruby is used within the test cases just to validate your answer.
+ */
+INSERT INTO
+  participants(name, age, attending)
+VALUES
+('Vitaly', 29, true);
+
+SELECT
+  *
+FROM
+  participants;
