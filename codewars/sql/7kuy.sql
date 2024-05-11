290729a554b28d7a7124fe8d3799137f9f33329f
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-11 21:41:35 +0300

    add solution #8 - 7kyu sql

--- codewars/sql/7kuy.sql
+++ codewars/sql/7kuy.sql
@@ -202,3 +202,38 @@ WHERE
   AND producent = 'CompanyA'
 ORDER BY
   id;
+
+/* 8. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/5a8ed96bfd8c066e7f00011a/train/sql
+ 
+ GROCERY STORE: Support Local Products
+ 
+ You are the owner of the Grocery Store. All your products are in the database, that you have created after CodeWars SQL excercises!:)
+ You care about local market, and want to check how many products come from United States of America or Canada.
+ Please use SELECT statement and IN to filter out other origins.
+ In the results show how many products are from United States of America and Canada respectively.
+ Order by number of products, descending.
+ 
+ products table schema
+ id
+ name
+ price
+ stock
+ producer
+ country
+ results table schema
+ products
+ country
+ */
+SELECT
+  country,
+  COUNT(*) AS products
+FROM
+  products
+WHERE
+  country IN('United States of America', 'Canada')
+GROUP BY
+  country
+ORDER BY
+  products DESC;
