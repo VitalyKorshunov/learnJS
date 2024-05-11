59931c90272cec8c545ee650ec913a7c65f80fd5
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-05-11 21:09:49 +0300

    add solution #6 - 7kyu sql

--- codewars/sql/7kuy.sql
+++ codewars/sql/7kuy.sql
@@ -143,3 +143,27 @@ ORDER BY
   capital
 LIMIT
   3;
+
+/* 6. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/59440034e94fae05b2000073/train/sql
+ 
+ SQL: Concatenating Columns
+ 
+ Given the table below:
+ ** names table schema **
+ id
+ prefix
+ first
+ last
+ suffix
+ 
+ Your task is to use a select statement to return a single column table containing the full title of the person (concatenate all columns together except id), as follows:
+ ** output table schema **
+ title
+ Don't forget to add spaces.
+ */
+SELECT
+  prefix || ' ' || first || ' ' || last || ' ' || suffix AS title
+FROM
+  names;
