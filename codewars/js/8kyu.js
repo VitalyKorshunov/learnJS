7dcaac2d8e434e920ee15f46ba7e3f9123f67b0e
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-06-05 18:26:42 +0300

    edit solution #42 - 8kyu

--- codewars/js/8kyu.js
+++ codewars/js/8kyu.js
@@ -1164,6 +1164,43 @@ function arrayPlusArray(arr1, arr2) {
   function sumArray(array) {
     return array.reduce((sum, n) => sum + n, 0);
   }
-  
+
   return sumArray(arr1) + sumArray(arr2);
+}
+
+
+
+// 42. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/javascript
+
+Correct the mistakes of the character recognition software
+
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, 
+searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition 
+softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function correct(string) {
+  const correctLetters = {
+    '5': 'S',
+    '0': 'O',
+    '1': 'I'
+  };
+
+  return string
+    .split('')
+    .map((letter) => (correctLetters[letter] !== undefined) ? correctLetters[letter] : letter)
+    .join('');
 }
