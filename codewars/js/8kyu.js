2b5c7eb4dc02f9579df6a0c2d3d90a02966c571d
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-06-05 18:02:13 +0300

    edit solution #41 - 8kyu

--- codewars/js/8kyu.js
+++ codewars/js/8kyu.js
@@ -1143,3 +1143,27 @@ function countBy(x, n) {
 
   return z;
 }
+
+
+
+// 41. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript
+
+Array plus array
+
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. 
+I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function arrayPlusArray(arr1, arr2) {
+  function sumArray(array) {
+    return array.reduce((sum, n) => sum + n, 0);
+  }
+  
+  return sumArray(arr1) + sumArray(arr2);
+}
