dd1a29e83138c72671653c4f431928d0349ecb34
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-06-17 19:36:41 +0300

    edit solution #51 - 7kyu

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -2048,6 +2048,30 @@ have to decide how to order multiple strings of the same length.
 
 // SOLUTION ✅✅✅✅✅✅
 
-function sortByLength (array) {
-  return array.slice().sort((a, b) => a.length - b.length);
+function sortByLength(array) {
+	return array.slice().sort((a, b) => a.length - b.length);
+}
+
+
+
+// 51. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/534ea96ebb17181947000ada/train/javascript
+
+Breaking chocolate problem
+
+Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of
+size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+
+For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one
+break, but for size 3 x 1 you must do two breaks.
+
+If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate
+to split). Input will always be a non-negative integer.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function breakChocolate(n,m) {
+  return (n > 0 && m > 0) ? n * m - 1 : 0;
 }
