6a8a92e6d8564e272b3f19a88bdafaca6e888e3a
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-06-19 23:05:36 +0300

    edit solution #43 - 8kyu

--- codewars/js/8kyu.js
+++ codewars/js/8kyu.js
@@ -1204,3 +1204,23 @@ function correct(string) {
     .map((letter) => (correctLetters[letter] !== undefined) ? correctLetters[letter] : letter)
     .join('');
 }
+
+
+
+// 43. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+
+Fake Binary
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'.
+Return the resulting string.
+
+Note: input will never be an empty string
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function fakeBin(x) {
+  return x.split('').map(x => (x < 5) ? 0 : 1).join('');
+}
