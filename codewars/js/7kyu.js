636f070c49f6a36fc6aadea8da6aaa65b5cc9249
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-06-27 14:30:52 +0300

    add solution #53 - 7kyu

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -2113,3 +2113,28 @@ function printerError(s) {
 	const errors = s.split('').reduce((n, char) => ('abcdefghijklm'.includes(char)) ? n : ++n, 0);
 	return `${errors}/${s.length}`;
 }
+
+
+
+// 53. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+
+Binary Addition
+
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, 
+or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function addBinary(a, b) {
+	return (a + b).toString(2);
+}
