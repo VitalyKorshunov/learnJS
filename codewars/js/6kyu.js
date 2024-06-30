70a2b570f94e368c2db478585d62c32b5e035e79
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-06-30 22:51:38 +0300

    add solution #25 - 6kyu

--- codewars/js/6kyu.js
+++ codewars/js/6kyu.js
@@ -894,3 +894,46 @@ function findMissingLetter(array) {
 		if (array.pop() != char) return char;
 	};
 }
+
+
+
+// 25. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/javascript
+
+Build Tower
+
+Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+	"  *  ",
+	" *** ", 
+	"*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+	"     *     ", 
+	"    ***    ", 
+	"   *****   ", 
+	"  *******  ", 
+	" ********* ", 
+	"***********"
+]
+Go challenge Build Tower Advanced once you have finished this :)
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function towerBuilder(nFloors) {
+	const res = [];
+
+	for (let i = 1; i <= nFloors; i++) {
+		res.push(' '.repeat(nFloors - i) + '*'.repeat(1 + (i - 1) * 2) + ' '.repeat(nFloors - i));
+	}
+
+	return res;
+}
