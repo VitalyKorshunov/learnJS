68321e19e6dc3b9ffb4ef6971cdc08a8a7b1b37a
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-07-01 23:14:32 +0300

    add solution #14 - 8kyu SQL

--- codewars/sql/8kuy.sql
+++ codewars/sql/8kuy.sql
@@ -308,3 +308,24 @@ FROM
   companies
 ORDER BY
   employees DESC;
+
+/* 14. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/594a6133704e4daf5d00003d/train/sql
+ 
+ Easy SQL: Rounding Decimals
+ 
+ Given the following table 'decimals':
+ 
+ ** decimals table schema **
+ 
+ id
+ number1
+ number2
+ Return a table with two columns (number1, number2), the value in number1 should be rounded down and the value in number2 should be rounded up.
+ */
+SELECT
+  FLOOR(number1) AS number1,
+  CEILING(number2) AS number2
+FROM
+  decimals;
