f2dfa6923c6cb06f8e1039fec48eef1b189099e8
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-07-02 23:58:03 +0300

    add solution #15 - 8kyu SQL

--- codewars/sql/8kuy.sql
+++ codewars/sql/8kuy.sql
@@ -329,3 +329,23 @@ SELECT
   CEILING(number2) AS number2
 FROM
   decimals;
+
+/* 15. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/594a9592704e4d21bc000131/train/sql
+ 
+ SQL Basics: Mod
+ 
+ Given the following table 'decimals':
+ 
+ ** decimals table schema **
+ 
+ id
+ number1
+ number2
+ Return a table with one column (mod) which is the output of number1 modulus number2.
+ */
+SELECT
+  MOD(number1, number2)
+FROM
+  decimals;
