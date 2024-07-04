53d3924da12af55289fff1ba3e5d390668fbca1c
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-07-04 22:49:06 +0300

    add solution #11 - 7kyu SQL

--- codewars/sql/7kuy.sql
+++ codewars/sql/7kuy.sql
@@ -310,3 +310,22 @@ FROM
   monsters
 ORDER BY
   id;
+
+/* 11. ❓❓❓❓❓❓
+ 
+ https://www.codewars.com/kata/594a8f653b5b4e8f3d000035/train/sql
+ 
+ SQL Basics: Raise to the Power
+ 
+ Given the following table 'decimals':
+ 
+ decimals table schema
+ id
+ number1
+ number2
+ Return a table with a single column result which is the output of number1 raised to the power of number2.
+ */
+SELECT
+  POW(number1, number2) AS result
+FROM
+  decimals;
