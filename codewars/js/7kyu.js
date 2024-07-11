b8a261b1e0e3881e4308a5491a254b982feced66
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-07-11 22:44:14 +0300

    add solution #60 - 7kyu JS

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -2274,3 +2274,34 @@ function arithmetic(a, b, operator) {
 
     return operations[operator];
 }
+
+
+// 60. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/586f6741c66d18c22800010a/train/javascript
+
+Sum of a sequence
+
+Your task is to write a function which returns the sum of a sequence of integers.
+The sequence is defined by 3 non-negative values: begin, end, step.
+If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+const sequenceSum = (begin, end, step) => {
+    let res = 0;
+
+    for (let i = begin; i <= end; i += step) {
+        res += i;
+    }
+
+    return res;
+};
