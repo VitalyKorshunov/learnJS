c90c4754ad60771f72871ce52ae2eab040929685
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-07-13 21:58:48 +0300

    add solution #61 - 7kyu JS

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -2305,3 +2305,23 @@ const sequenceSum = (begin, end, step) => {
 
     return res;
 };
+
+
+// 61. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+Sort Numbers
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function solution(nums){
+    return (nums === null) ? [] : nums.slice().sort((a, b) => a - b);
+}
