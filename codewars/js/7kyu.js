0f53990b9a2d459f6d426be5c838831919566da2
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-07-14 22:58:36 +0300

    add solution #62 - 7kyu JS

--- codewars/js/7kyu.js
+++ codewars/js/7kyu.js
@@ -2322,6 +2322,23 @@ solution(null); // should return []
 
 // SOLUTION ✅✅✅✅✅✅
 
-function solution(nums){
+function solution(nums) {
     return (nums === null) ? [] : nums.slice().sort((a, b) => a - b);
+}
+
+
+// 62. ❓❓❓❓❓❓
+/*
+https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+
+Small enough? - Beginner
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+You can assume all values in the array are numbers.
+*/
+
+// SOLUTION ✅✅✅✅✅✅
+
+function smallEnough(a, limit) {
+    return a.every((e) => e <= limit);
 }
